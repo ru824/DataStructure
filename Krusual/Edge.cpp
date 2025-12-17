bab79415bd7e96d6b8b7c7ff8b4116c61326f2ba
ru824 <13853431868@163.com>
2025-12-17 13:53:16 +0800

    删除冗余文件

--- Krusual/Edge.cpp
+++
@@ -1,9 +0,0 @@
-#include "Edge.h"
-
-// ¹¹Ôìº¯Êı
-Edge::Edge(int s, int d, int w) : src(s), dest(d), weight(w) {}
-
-int Edge::getSrc() const { return src; }
-int Edge::getDest() const { return dest; }
-int Edge::getWeight() const { return weight; }
-void Edge::setWeight(int w) { weight = w; }
